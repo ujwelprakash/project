fd165116d6d3afa2e260e6230233e8e17acd52f7
ujwelprakash <ujwelprakash@gmail.com>
2025-02-13 12:57:19 +0530

    Update read.js

--- src/pages/read.js
+++ src/pages/read.js
@@ -11,39 +11,28 @@ function Read() {
     }, [])
 
 
-    return ( < div className = 'd-flex w-100 justify-content-center align-items-center bg-light' >
-        <
-        div className = 'w-50 border bg-white shadow px-5 pt-3 pb-5 rounded' >
-
-        <
-        table className = "table" >
-        <
-        thead >
-        <
-        tr >
-        <
-        th > Name < /th> <
-        th > Email < /th> <
-        th > Action < /th> <
-        /tr> <
-        /thead> <
-        tbody > {
-            estimate.map(user => ( <
-                tr key = { user._id } >
-                <
-                td > { user.name } < /td> <
-                td > { user.email } < /td>
-
-                <
-                /tr>
-            ))
-        } <
-        /tbody> <
-        /table>
-
-        <
-        /div>  <
-        /div>
+    return ( <div className="d-flex w-100 justify-content-center align-items-center bg-light">
+        <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {estimate.map(user => (
+                <tr key={user._id}>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  {/* You can add the Action column content here */}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     )
 }
 
