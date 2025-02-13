d1b568a749855af988d84b3fa754620a7fdfaa82
ujwelprakash <ujwelprakash@gmail.com>
2025-02-13 12:59:29 +0530

    Update userlogin.js

--- src/pages/userlogin.js
+++ src/pages/userlogin.js
@@ -26,113 +26,73 @@ function Customer() {
     }
 
     return (
+<div className="container-fluid">
+  <div className="row">
+    <div className="rect">
+      <div className="row">
+        <div className="mx-auto recttwo">
+          <div className="row">
+            <div className="relogin"></div>
 
-        <
-        div className = "container-fluid" >
+            <div className="data">
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="email" style={{ paddingTop: "170px", paddingRight: "300px" }}>
+                  Email:
+                </label>
+                <br />
+                <input
+                  style={{ marginLeft: "60px", width: "300px" }}
+                  type="email"
+                  placeholder="Enter Email"
+                  autoComplete="off"
+                  name="email"
+                  className="form-control rounded-0"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <br />
 
-        <
-        div className = "row" >
+                <label htmlFor="password" style={{ paddingTop: "50px", paddingRight: "280px" }}>
+                  Password:
+                </label>
+                <br />
+                <input
+                  style={{ marginLeft: "60px", width: "300px" }}
+                  type="password"
+                  placeholder="Enter Password"
+                  autoComplete="off"
+                  name="password"
+                  className="form-control rounded-0"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <br />
 
-        <
-        div className = "rect" >
+                <button
+                  style={{
+                    width: "100px",
+                    backgroundColor: "orange",
+                    borderRadius: "15px",
+                  }}
+                  type="submit"
+                  className="btn w-50"
+                >
+                  Login
+                </button>
+                <br />
+                <br />
+              </form>
 
-        <
-        div className = "row" >
-
-        <
-        div className = "mx-auto recttwo" >
-
-        <
-        div className = "row" >
-
-        <
-        div className = "relogin" > < /div>
-
-        <
-        div className = "data" >
-
-        <
-        form onSubmit = { handleSubmit } >
-
-        <
-        label htmlFor = "email"
-        style = {
-            { paddingTop: "170px", paddingRight: "300px" } } > Email: < /label> <br/ >
-
-        <
-        input style = {
-            { marginLeft: "60px", width: "300px" } }
-        type = "email"
-        placeholder = "Enter Email"
-        autoComplete = "off"
-        name = "email"
-        className = "form-control rounded-0"
-        onChange = {
-            (e) => setEmail(e.target.value)
-        }
-        />
-
-        <
-        br / >
-
-        <
-        label htmlFor = "password"
-        style = {
-            { paddingTop: "50px", paddingRight: "280px" }
-        } > Password: < /label> <br/ >
-
-        <
-        input style = {
-            { marginLeft: "60px", width: "300px" }
-        }
-        type = "password"
-        placeholder = "Enter Password"
-        autoComplete = "off"
-        name = "password"
-        className = "form-control rounded-0"
-        onChange = {
-            (e) => setPassword(e.target.value)
-        }
-        />
-
-        <
-        br / > < br / >
-
-        <
-        button style = {
-            { width: "100px", backgroundColor: "orange", borderRadius: "15px 15px 15px 15px" }
-        }
-        type = "submit"
-        className = "btn w-50 " > Login < /button> <br/ > < br / >
-
-        <
-        /form> <p > Don "t have account?</p >
-        <
-        Link to = "/Register"
-        style = {
-            { backgroundColor: "lightgrey" } }
-        className = "btn btn-default border w-20 rounded-10 " > Register < /Link> <
-        /div>
-
-
-
-        <
-        /div> 
-
-        <
-        /div>
-
-        <
-        /div>
-
-        <
-        /div>
-
-        <
-        /div>
-
-        <
-        /div>
+              <p>Don't have an account?</p>
+              <Link to="/Register" style={{ backgroundColor: "lightgrey" }} className="btn btn-default border w-20 rounded-10">
+                Register
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
     )
 }
