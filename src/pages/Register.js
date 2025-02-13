1974cb845c84406d8a723654f7840ea60c68cf64
ujwelprakash <ujwelprakash@gmail.com>
2025-02-13 12:58:39 +0530

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -44,121 +44,92 @@ function Register() {
 
 
     return (
-
-
-        <
-        div className = "container-fluid" >
-
-        <
-        div className = "row" >
-
-        <
-        div className = "rect" >
-
-        <
-        div className = "row" >
-
-        <
-        div className = "mx-auto recttwo" >
-
-        <
-        div className = "row" >
-
-        <
-        div className = "image" > < /div>
-
-        <
-        div className = "data" >
-        <
-        form onSubmit = { handleSubmit } >
-
-        <
-        label style = {
-            { paddingTop: "130px", paddingRight: "300px" } }
-        htmlFor = "name" > name: < /label> <br/ >
-
-        <
-        input style = {
-            { width: "300px", marginLeft: "60px" } }
-        type = "text"
-        placeholder = "Enter Name"
-        autoComplete = "off"
-        name = "name"
-        className = "form-control rounded-0"
-        onChange = {
-            (e) => setName(e.target.value) }
-        /> < br / > < br / >
-
-
-        <
-        label style = {
-            { paddingRight: "300px" } }
-        htmlFor = "email" > email < /label> <br/ >
-
-        <
-        input style = {
-            { width: "300px", marginLeft: "60px" } }
-        type = "email"
-        placeholder = "Enter Email"
-        autoComplete = "off"
-        name = "email"
-        className = "form-control rounded-0"
-        onChange = {
-            (e) => setEmail(e.target.value) }
-        />< br / > < br / >
-
-        <
-        label style = {
-            { paddingRight: "270px" } }
-        htmlFor = "password" > password < /label>
-
-        <
-        input style = {
-            { width: "300px", marginLeft: "60px" } }
-
-        type = "password"
-        placeholder = "Enter Password"
-        autoComplete = "off"
-        name = "password"
-        className = "form-control rounded-0"
-        onChange = {
-            (e) => setPassword(e.target.value) }
-        />   <br / > < br / >
-
-        <
-        button style = {
-            { backgroundColor: "orange", borderRadius: "15px 15px 15px 15px" } }
-        type = "submit"
-        className = "btn  w-50 " > Register < /button>
-
-        <
-        /form> <
-        p > Already have account ? < /p> <
-        Link to = "/Customer"
-        style = {
-            { backgroundColor: "lightgrey" } }
-        className = "btn btn-default border w-20 rounded-10 " > Login < /Link> 
-
-        <
-        /div>  <
-        /div> 
-
-        <
-        /div>
-
-        <
-        /div>
-
-        <
-        /div>
-
-        <
-        /div>
-
-        <
-        /div>
-
-
+<div className="container-fluid">
+        <div className="row">
+          <div className="rect">
+            <div className="row">
+              <div className="mx-auto recttwo">
+                <div className="row">
+                  <div className="image"></div>
+      
+                  <div className="data">
+                    <form onSubmit={handleSubmit}>
+                      <label style={{ paddingTop: "130px", paddingRight: "300px" }} htmlFor="name">
+                        Name:
+                      </label>
+                      <br />
+                      <input
+                        style={{ width: "300px", marginLeft: "60px" }}
+                        type="text"
+                        placeholder="Enter Name"
+                        autoComplete="off"
+                        name="name"
+                        className="form-control rounded-0"
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                      <br />
+                      <br />
+      
+                      <label style={{ paddingRight: "300px" }} htmlFor="email">
+                        Email:
+                      </label>
+                      <br />
+                      <input
+                        style={{ width: "300px", marginLeft: "60px" }}
+                        type="email"
+                        placeholder="Enter Email"
+                        autoComplete="off"
+                        name="email"
+                        className="form-control rounded-0"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <br />
+                      <br />
+      
+                      <label style={{ paddingRight: "270px" }} htmlFor="password">
+                        Password:
+                      </label>
+                      <br />
+                      <input
+                        style={{ width: "300px", marginLeft: "60px" }}
+                        type="password"
+                        placeholder="Enter Password"
+                        autoComplete="off"
+                        name="password"
+                        className="form-control rounded-0"
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      <br />
+                      <br />
+      
+                      <button
+                        style={{
+                          backgroundColor: "orange",
+                          borderRadius: "15px",
+                        }}
+                        type="submit"
+                        className="btn w-50"
+                      >
+                        Register
+                      </button>
+                    </form>
+      
+                    <p>Already have an account?</p>
+                    <Link
+                      to="/Customer"
+                      style={{ backgroundColor: "lightgrey" }}
+                      className="btn btn-default border w-20 rounded-10"
+                    >
+                      Login
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
 
     )
 }
